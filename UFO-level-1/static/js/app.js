4243932b008b96dcbe56972df9b2d1077ff387b5
Jonathan <jondtran132@gmail.com>
2021-06-23 15:43:51 -0700

    reverted level 2 duplicate to level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,31 +12,9 @@ function filterTable() {
 
     d3.selectAll("tbody>tr").remove();
 
-    var filters = {
-        datetime: d3.select("#datetime").property("value"),
-        city: d3.select("#city").property("value"),
-        state: d3.select("#state").property("value"),
-        country: d3.select("#country").property("value"),
-        shape: d3.select("#shape").property("value")
-    };
+    var inputValue = d3.select("#datetime").property("value");
 
-    var activeFilters = [];
-
-    for (i=0; i<Object.keys(filters).length; i++) {
-        if (Object.values(filters)[i] != "") {
-            activeFilters.push(Object.keys(filters)[i]);
-        }
-    };
-
-    var filteredData = tableData.filter(function(item) {
-        for (i=0; i<activeFilters.length; i++) {
-            input = activeFilters[i];
-            if (item[input] != filters[input]) {
-                return false;
-            }
-        }
-        return true;
-    });
+    var filteredData = tableData.filter(item => item.datetime === inputValue);
 
     var tbody = d3.select("tbody");
 
